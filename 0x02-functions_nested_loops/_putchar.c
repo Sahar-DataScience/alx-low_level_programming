e5657b03a85865b52335522338356a17145f9605
Sahar <sahar.ben.mabrouk.pro@gmail.com>
2023-07-21 12:17:00 -0700

    new trial

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 
 /* build the fuction that prints putchar followed by newline */
 
-int _putchar(void)
+int _putchar(void) /* function declaration prototype*/
 {
         printf("_putchar\n");
         return(0);
