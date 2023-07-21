73f4e2d68a300f927f65872d5af4732ccd983cb4
Sahar <sahar.ben.mabrouk.pro@gmail.com>
2023-07-21 07:02:21 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/* 
+ * make function that prints _putchar 
+ * followed be new line 
+ */
+
+/* function declaration */
+
+int main()
+{
+	printf("_putchar\n");
+	return(0);
+}
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nested loops tutorial in c
