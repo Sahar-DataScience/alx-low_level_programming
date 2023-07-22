b5616a0f0f52cd150365970671adec259e617250
Sahar <sahar.ben.mabrouk.pro@gmail.com>
2023-07-22 13:52:58 -0700

    no space betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-/** _isupper :  checks for uppercase character
+/**
+ *  _isupper -  checks for uppercase character
  * @c : the input char to check
+ *
  * Return 1 if uppercase and 0 otherwise
 */
 
 int _isupper(int c)
 {
-	if ( c >= 65 && c <= 90 )
+	if (c >= 65 && c <= 90)
 	{
 
 		return (1);
