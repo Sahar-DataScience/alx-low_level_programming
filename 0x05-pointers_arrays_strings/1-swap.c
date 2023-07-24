de6db6c5090fe08a0894bf62ed16ce1e77a7b313
Sahar <sahar.ben.mabrouk.pro@gmail.com>
2023-07-24 13:44:29 -0700

    switch vars values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int -  swaps the values of two integers.
+ *
+ * @a @b : pointers of integers
+ *
+ * Return void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
