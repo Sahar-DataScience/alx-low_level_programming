3e2b603b4c4d744977a44706be058dff66b5b7a7
Sahar <sahar.ben.mabrouk.pro@gmail.com>
2023-08-01 11:58:39 -0700

    strsp

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - Returns the number of bytes in the initial segment of s which consist only of bytes from accept.
+ * @s: source string
+ * @accept: searching string
+ *
+ * Return: new string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); j++)
+	{
+		for (i = 0; *(accept + i); i++)
+		{
+			if (*(s + j) == *(accept + i))
+				break;
+		}
+	if (*(accept + i) == '\0')
+		break;
+	}
+	return (j);
+}
