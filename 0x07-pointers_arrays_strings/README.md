d6668bf45fc1b9b6aaab5fb4ce9dda58e7275ec3
Sahar <sahar.ben.mabrouk.pro@gmail.com>
2023-08-01 11:19:48 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+tutorial about double pointers and multi-dim arrays
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+
+#endif
