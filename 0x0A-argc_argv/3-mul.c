72701bb38b233dfaaa819bb706ea60dcc82a59cf
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2023-10-03 06:19:30 -0700

    multiply two argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/** main- print the result of the multiplication, followed by a new line
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+	if(argc == 3)
+	{
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", m);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
